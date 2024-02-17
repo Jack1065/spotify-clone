82c98ffd0511f31dbf36edc3d1adb8da06dbc35c
Jack1065 <115672450+Jack1065@users.noreply.github.com>
2024-02-17 16:08:32 -0600

    Search Bar
    button justified left

--- spotify-clone/src/App.js
+++ spotify-clone/src/App.js
@@ -1,24 +1,47 @@
 import logo from './logo.svg';
 import './App.css';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import {Container, InputGroup, FormControl ,Button, Row, Card} from 'react-bootstrap';
+import {useState, useEffect} from 'react';
 
 function App() {
+  const[searchInput,setSearchInput] = useState("");
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <body style={{backgroundColor: "#D3D3D3"}}>
+    <div className="App"
+    style = {{
+      
+    }}>
+      <Container>
+        <InputGroup className="mb-3" size="lg">
+          <FormControl 
+        
+          placeholder="Search Music"
+          type = "input"
+          onKeyPress ={event=> { 
+            if(event.key === "Enter"){
+              console.log("Pressed Enter");
+            }
+          }}
+          
+          onChange={event=> setSearchInput(event.target.value)}
+          />
+          <Button onClick={()=>{console.log("Hello")}} style={{backgroundColor:"black"}}>
+            Search
+          </Button>
+        </InputGroup>
+      </Container>
+      <Container>
+        <Card>
+          <Card.Img src="#" />
+          <Card.Body>
+            <Card.Title>Album Name</Card.Title>
+          </Card.Body>
+        </Card>
+      </Container>
     </div>
+    </body>
   );
 }
 
